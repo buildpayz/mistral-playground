ba98bc5843ef6f4f62582f4c398df1cc1be45fe7
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2025-03-14 14:22:59 +0530

    gemini api added

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ import os
 from mistralai import Mistral
 from dotenv import load_dotenv
 import streamlit as st
+from google import genai
+from google.genai import types
 
 load_dotenv('./.env')
 
 # Retrieve the API key from environment variables
-api_key = os.environ["MISTRAL_API_KEY"]
+mistral_api_key = os.environ["MISTRAL_API_KEY"]
+gemini_api_key = os.environ["GEMINI_API_KEY"]
 
 # Specify model
-model = "mistral-small-latest"
+# model = "mistral-large-latest"
 
 # Initialize the Mistral client
-client = Mistral(api_key=api_key)
+mistral_client = Mistral(api_key=mistral_api_key)
+gemini_client = genai.Client(api_key=gemini_api_key)
 
-st.title("Mistral Playground")
 
-system_prompt = st.text_input("System prompt:",placeholder="What this document is about?")
+st.title("Quotation Playground")
+
+system_prompt = st.text_area("System prompt:",placeholder="What this document is about?")
 user_prompt = st.text_area("User prompt:", placeholder="What you want to see from this document?.")
 file_path = st.text_input("File Path:", placeholder="Online pdf link")
 
@@ -32,36 +37,26 @@ if st.button("Get Response"):
         st.stop()
     
     # Define the messages for the chat
-    messages = [
-        {
-            "role": "system",
-            "content": [
-                {
-                    "type": "text",
-                    "text": system_prompt
-                },
-            ]
-        },
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": user_prompt
-                },
-                {
-                    "type": "document_url",
-                    "document_url": file_path
-                }
-            ]
-        }
-    ]
 
-    # Get the chat response
-    chat_response = client.chat.complete(
-        model=model,
-        messages=messages
+    ocr_response = mistral_client.ocr.process(
+        model="mistral-ocr-latest",
+        document={
+            "type": "document_url",
+            "document_url": file_path
+        },
+        include_image_base64=True
     )
 
-    # Print the content of the response
-    st.write(chat_response.choices[0].message.content)
+    response = gemini_client.models.generate_content(
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt),
+        contents=[f"{user_prompt}", f"{ocr_response}"],
+    )
+
+
+    
+    st.write(f"""{response.candidates[0].content.parts[0].text[3:-3]}""")
+    with open("output.md", "w") as file:
+        file.write(response.candidates[0].content.parts[0].text[3:-3])
+    st.success("Response written to output.md")
